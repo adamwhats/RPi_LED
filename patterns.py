07342bf3ee36b8e64e4be829d96fc42ac817d355
adam.watts <adam.watts@the-mtc.org>
2022-01-05 17:05:34 +0000

    Added smoother transitions between patterns

--- patterns.py
+++ patterns.py
@@ -5,16 +5,34 @@ from rpi_ws281x import Color
 
 def color_wipe(strip, lp):
     """Wipe color across display a pixel at a time."""
-    colour = tuple
+    # Initialise
+    colour = int
+    i = 0
     if lp.active:
         colour = Color(*lp.colour)
     else:
         colour = Color(0, 0, 0)
-    for i in range(strip.numPixels()):
+    # Colour
+    while i < strip.numPixels():
         strip.setPixelColor(i, colour)
         strip.show()
         time.sleep(10 / 1000.0)
-        if not lp.pattern == "block":
+        i += 1
+        # Update on lp.active change
+        if lp.active and colour != Color(*lp.colour):
+            colour = Color(*lp.colour)
+            i = 0
+        if not lp.active and colour != Color(0, 0, 0):
+            colour = Color(0, 0, 0)
+            i = 0
+        # Transition to rainbow pattern
+        if lp.pattern == "rainbow" and lp.active:
+            for i in range(strip.numPixels()):
+                colour = wheel(int(i * 256 / strip.numPixels() * lp.width) & 255)
+                strip.setPixelColor(i, colour)
+                strip.show()
+                time.sleep(10 / 1000.0)
+            lp.fade = 255
             break
 
 
@@ -32,17 +50,36 @@ def wheel(pos):
 
 def rainbow_cycle(strip, lp, iterations=1):
     """Draw rainbow that uniformly distributes itself across all pixels."""
-    for j in range(int(256 * iterations)):
-        for i in range(strip.numPixels()):
-            strip.setPixelColor(i, wheel((int(i * 256 / strip.numPixels() * lp.width) + j) & 255))
-        if not lp.active:
-            [strip.setPixelColor(i, Color(0, 0, 0)) for i in range(strip.numPixels())]
-            strip.show()
-            break
-        elif lp.pattern != "rainbow":
+    # Initialise
+    frame_count = 0
+    j = 0
+    # Colour
+    while j < int(256 * iterations):
+        if frame_count % lp.speed == 0:
+            for i in range(strip.numPixels()):
+                colour = wheel((int(i * 256 / strip.numPixels() * lp.width) + j) & 255)
+                strip.setPixelColor(i, colour)
+                strip.setBrightness(255)
+            j += 1
+        # Fade
+        if lp.active:  # Fade on
+            for i in range(strip.numPixels()):
+                strip.setBrightness(int(lp.fade))
+            lp.fade = min(lp.fade + 2, 255)
+        elif not lp.active:  # Fade off
+            for i in range(strip.numPixels()):
+                strip.setBrightness(int(lp.fade))
+            lp.fade = min(lp.fade - 2, 255)
+            if lp.fade <= 0:  # Set to black
+                for i in range(strip.numPixels()):
+                    strip.setPixelColor(i, Color(0, 0, 0))
+                strip.show()
+                break
+        if lp.pattern != "rainbow":
             break
         strip.show()
-        time.sleep(lp.speed / 1000.0)
+        frame_count += 1
+        time.sleep(0.001)
 
 
 if __name__ == "__main__":
